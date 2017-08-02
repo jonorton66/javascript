85201abc7282ef6254dec70406535483664e8ea7
Jon Orton <jonorton66@gmail.com>
2017-08-01 21:05:36 -0400

    changed some syntax in DateTime.js

--- basics/DateTime.js
+++ basics/DateTime.js
@@ -4,7 +4,7 @@ var today = new Date();
 
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+var dateTime = date + ' ' + time;
 
 return dateTime;
 }
