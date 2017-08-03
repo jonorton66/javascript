883f2c47c6ee25aa314cfd3e27b887522edbeb77
Jon Orton <jonorton66@gmail.com>
2017-08-02 20:18:35 -0400

    Various: Comments, fixes to passing variables, added a division function

---
+++ basics/MultiplyDivide.js
@@ -0,0 +1,14 @@
+//Function to handle the multiplication
+function multiply(){
+  var x = document.getElementById('x').value;
+  var y = document.getElementById('y').value;
+
+  document.getElementById("prod").innerHTML = x * y;
+}
+//function to handle the division
+function divide(){
+  var x = document.getElementById('x').value;
+  var y = document.getElementById('y').value;
+
+  document.getElementById("div").innerHTML = x / y;
+}
--- basics/multiplication.js
+++
@@ -1,8 +0,0 @@
-function mult(){
-  let x = "1";
-  let y = "5";
-
-  return x*y;
-}
-
-document.write(mult())
